cded79c051acfb460808b469b2c0f82da85aabde
Gabriel Pontes <33611916+ograndoptimist@users.noreply.github.com>
2019-03-03 11:52:15 -0300

    Update agent.py

--- agent.py
+++ agent.py
@@ -8,7 +8,14 @@ class DeepQLearningAgent(object):
     def __init__(self):
         self.model = self.modelo()
 
-    def modelo(self):
+    def modelo(self, estado_texto, acao_texto, dimensoes_embedding = 16, dimensoes_lstm = 32, numero_maximo_palavras):
+        model = Sequential()
+
+        model.add(Embedding(numero_maximo_palavras, dimensoes_embedding))
+        model.add(LSTM(dimensoes_lstm))
+        model.add(Dense(8))        
+
+        model.compile(optimizer = 'rmsprop', loss_function = '', metrics = '')
         
         return model
 
@@ -21,12 +28,10 @@ class DeepQLearningAgent(object):
                 if epsilon < np.random.random():
                     executa =
                 else:
-                    target = 
-                    self.model.fit(estado, target, epochs = 10, verbose = False)
                     acao = self.model.predict(estado)
                 proximo_estado, proxima_acao, reforco, terminado = jogo.transicao_estado(acao)
+                target = self.Q(estado, acao) + learning_rate * [self.Q(proximo_estado, proxima_acao) - self.Q(estado, acao)]
+                self.model.fit(estado, target, epochs = 10, verbose = False)
                 eps *= epsilon_decay
-                
-                
-            
-        
+
+    def Q(self, estado, acao):
