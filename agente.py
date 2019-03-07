e1a14c6e7914ada3c0165f01c35db2dd519c1c40
Gabriel Pontes <33611916+ograndoptimist@users.noreply.github.com>
2019-03-07 18:15:45 -0300

    Update agente.py

--- agente.py
+++ agente.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Embedding, Dense, LSTM
 from utilidades import preprocessamento, tokenizacao, palavraParaIndice, vetorizacao
+import random
 
 
 
@@ -28,9 +29,9 @@ class DeepQLearningAgent(object):
 
         model.add(Embedding(numero_maximo_palavras, dimensoes_embedding))
         model.add(LSTM(dimensoes_lstm))
-        model.add(Dense(8))        
+        model.add(Dense(8, activation = 'tanh'))        
 
-        model.compile(optimizer = 'rmsprop', loss_function = '', metrics = ['acc'])
+        model.compile(optimizer = 'rmsprop', loss_function = 'mse', metrics = ['acc'])
         
         return model
 
@@ -45,13 +46,51 @@ class DeepQLearningAgent(object):
         tokens = tokenizacao(texto)
         vetor = vetorizacao(tokens, self.dicionario_de_tokens)
 
-        return vetor       
+        return vetor
 
-    def treino(self, episodios: int, epsilon: float, epsilon_decay: float, taxa_aprendizado: float, fator_desconto: float):
+    def q_value(self, estado, acao):
+        """
+            Alimenta o modelo com um estado e uma ação, de forma a obter na saída o Q(s, a).
+                ::estado:
+                ::acao:
+        """
+        return self.model.predict([estado + acao])        
+
+    def acao(estado, acoes, epsilon, funcao_historico):
+        """
+            Seletor de ações.
+            ::estado:
+            ::acoes:
+            ::epsilon:
+                Probabilidade de escolher uma ação aleatória.
+            ::historico:
+                retorna um valor igual ao número de vezes o agente selecionou a ação a no estado s na atual execução.
+                o histórico penaliza os pares estado-ação já visitados.
+        """
+    
+        if np.random.random() < epsilon:
+            return random.randint(0, len(espaco_acoes) + 1)
+
+        q_values = [self.q_value(estado, acao) for acao in acoes]
+
+        # normaliza Q-values de [-1, 1] para [0, 1]
+        q_values = (q_values + 1) / 2
+
+        # Aplica a função histórico
+        q = funcao_historico(s, a) + 1 for q in q_values
+
+        # normaliza Q-values de [0, 1] para [-1, 1]
+        q_values = (q_values * 2) - 1
+
+        return max(q_values)
+
+    def treino(self, episodios = 256: int, epsilon = 1.0: float, epsilon_decay = 0.99: float,
+                   taxa_aprendizado = 0.0002: float, gamma = 0.95: float):
         """
             Realiza o treinamento do Agente.
         """
         eps = epsilon
+        estat_reforcos = dict()
         for episodio in range(1, episodios + 1):
             jogo = AdmiravelMundoNovo()
 
@@ -69,9 +108,9 @@ class DeepQLearningAgent(object):
                 proximo_estado_texto, proxima_acao_texto, reforco, prox_dimensao_acao, terminado = jogo.transicao_estado(acao)
                 proximo_estado, proxima_acao = self.transforma(proximo_estado_texto, proxima_acao_texto)                
 
-                target = reforco + fator_desconto * self.Q(proximo_estado, proxima_acao)
+                Q_target = reforco + gamma * self.Q(proximo_estado, proxima_acao)
 
-                self.model.fit(estado, target, epochs = 10, verbose = False)
+                self.model.fit(estado, Q_target, epochs = 10, verbose = False)
 
                 estado = proximo_estado
                 acao = proxima_acao
@@ -80,6 +119,15 @@ class DeepQLearningAgent(object):
                 reforco_acumulado += reforco
 
             print("Episódio {0}: Reforço acumulado de {1}".format(episodio, reforco_acumulado))
+            estat_reforcos = {episodio: reforco_acumulado}
 
     def Q(self, estado, acao):
+        """
+            Retorna a ação de maior Q-value.
+        """        
         pass
+
+
+if __name__ == '__main__':
+    agente = DeepQLearningAgente()
+    agente.treino()
